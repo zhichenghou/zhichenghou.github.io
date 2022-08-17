aad7f9b8331b5348455b954049c7b36a957973af
houzhicheng <houzhicheng@bytedance.com>
2022-08-17 21:00:13 +0800

    add first blog

--- src/views/posts/howToBuildThis.tsx
+++ src/views/posts/howToBuildThis.tsx
@@ -17,7 +17,7 @@ const HowToBuildThisSite: React.FC = () => {
         <h2>技术选型</h2>
         <p>
           前端框架选择了 React， 使用{' '}
-          <a href='Create React App' target='_blank'>
+          <a href='Create React App' target='_blank' rel='noreferrer'>
             Create React App
           </a>{' '}
           很方便的构建起了一个项目的基础，然后就可以写代码来构建页面了。
@@ -25,7 +25,7 @@ const HowToBuildThisSite: React.FC = () => {
         <p>
           对于 CSS 开始打算使用 UI 框架来着，但看了一圈都很复杂，转念一想，既然是练手和学习项目，那就使用纯 css
           来构建吧。在 v2ex 上发现了{' '}
-          <a href='https://andybrewer.github.io/mvp/' target='_blank'>
+          <a href='https://andybrewer.github.io/mvp/' target='_blank' rel='noreferrer'>
             mvp.css
           </a>{' '}
           ，这是一个最小化的 css 样式苦，文件只有几百行，没有 class name，只用 html 标签来指定样式，是非常简单易用。
@@ -36,7 +36,7 @@ const HowToBuildThisSite: React.FC = () => {
         <ul>
           <li>
             由于 react 组件的原因，对 mvp css 做了一处注释修改，否则网页 header 位置会有问题。
-            <code>/* div header, */</code>
+            <code>div header,</code>
           </li>
           <li>
             react-router 在 github pages 上存在刷新 404 的问题，解决办法是使用<code>HashRouter</code> 代替
